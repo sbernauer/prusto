82b20c2e533e8bd68fd3eeea2843622ec4c8912f
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2023-11-24 16:11:14 +0100

    formatting

--- tests/macros/presto.rs
+++ tests/macros/presto.rs
@@ -1,70 +1,70 @@
-use prusto::Presto;
 use prusto::tuples::*;
+use prusto::Presto;
 
 #[derive(Presto)]
-struct Person  {
+struct Person {
     name: String,
     age: i32,
 }
 
 #[derive(Presto)]
-struct Group  {
+struct Group {
     name: String,
     leader: Person,
 }
 
 #[derive(Presto)]
-struct Foo  {
+struct Foo {
     name: String,
     bar: i32,
 }
 
 #[derive(Presto)]
-struct Generic<T: Presto>  {
+struct Generic<T: Presto> {
     name: String,
     t: T,
 }
 
 #[derive(Presto)]
 struct BigStruct {
-  a1: i16,
-  a2: i16,
-  a3: i16,
-  a4: i16,
-  a5: i16,
-  a6: i16,
-  a7: i16,
-  a8: i16,
-  a9: i16,
-  a10: i16,
-  a11: i16,
-  a12: i16,
-  a13: i16,
-  a14: i16,
-  a15: i16,
-  a16: i16,
-  a17: i16,
-  a18: i16,
-  a19: i16,
-  a20: i16,
-  a21: i16,
-  a22: i16,
-  a23: i16,
-  a24: i16,
-  a25: i16,
-  a26: i16,
-  a27: i16,
-  a28: i16,
-  a29: i16,
-  a30: i16,
-  a31: i16,
-  a32: i16,
+    a1: i16,
+    a2: i16,
+    a3: i16,
+    a4: i16,
+    a5: i16,
+    a6: i16,
+    a7: i16,
+    a8: i16,
+    a9: i16,
+    a10: i16,
+    a11: i16,
+    a12: i16,
+    a13: i16,
+    a14: i16,
+    a15: i16,
+    a16: i16,
+    a17: i16,
+    a18: i16,
+    a19: i16,
+    a20: i16,
+    a21: i16,
+    a22: i16,
+    a23: i16,
+    a24: i16,
+    a25: i16,
+    a26: i16,
+    a27: i16,
+    a28: i16,
+    a29: i16,
+    a30: i16,
+    a31: i16,
+    a32: i16,
 }
 
 fn test_simple() {
     let p = Person {
         name: "h".to_string(),
-        age: 5
+        age: 5,
     };
 
     assert_eq!(p.value(), Tuple2(&"h".to_string(), &5));
@@ -75,11 +75,14 @@ fn test_nested() {
         name: "g1".to_string(),
         leader: Person {
             name: "h".to_string(),
-            age: 5
+            age: 5,
         },
     };
 
-    assert_eq!(g.value(), Tuple2(&"g1".to_string(), Tuple2(&"h".to_string(), &5)));
+    assert_eq!(
+        g.value(),
+        Tuple2(&"g1".to_string(), Tuple2(&"h".to_string(), &5))
+    );
 }
 
 fn test_generic() {
@@ -88,10 +91,13 @@ fn test_generic() {
         t: Foo {
             name: "foo".to_string(),
             bar: 10,
-        }
+        },
     };
 
-    assert_eq!(g.value(), Tuple2(&"gen".to_string(), Tuple2(&"foo".to_string(), &10)));
+    assert_eq!(
+        g.value(),
+        Tuple2(&"gen".to_string(), Tuple2(&"foo".to_string(), &10))
+    );
 }
 
 fn test_wrap() {
@@ -100,10 +106,9 @@ fn test_wrap() {
         a: u32,
     }
 
-    let a = A {a: 1};
+    let a = A { a: 1 };
     assert_eq!(a.value(), Tuple1(&1));
 }
-
 
 fn main() {
     test_simple();
